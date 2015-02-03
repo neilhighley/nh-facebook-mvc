0734f608fa7115a05587d03ad24aae01c2fde6b2
Neil Highley <neil@neilhighley.com>
2015-02-03 21:28:22 +0000

    added facebook page

--- src/neilhighley-fb/neilhighley-fb/Controllers/HomeController.cs
+++ src/neilhighley-fb/neilhighley-fb/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using neilhighley_fb.Models;
 
 namespace neilhighley_fb.Controllers
 {
@@ -26,6 +27,20 @@ namespace neilhighley_fb.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Facebook()
+        {
+            ViewBag.Message = "Your Facebook page.";
+
+            var fbm = new FacebookUserModel() {Name = User.Identity.Name};
+            fbm.Friends=new List<FriendModel>()
+            {
+                new FriendModel(){Name = "Neil",Profile=""},
+                new FriendModel(){Name="Fred",Profile = ""}
+            };
+
+            return View(fbm);
         }
     }
 }
