d4289f9e998731fb01496bd3d699b195d8c5706a
Neil Highley <neil@neilhighley.com>
2015-02-03 21:30:16 +0000

    added missing files

---
+++ src/neilhighley-fb/neilhighley-fb/Models/FacebookUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace neilhighley_fb.Models
+{
+    public class FacebookUserModel
+    {
+        public string Name { get; set; }
+        public List<FriendModel> Friends { get; set; }
+ 
+    }
+
+    public class FriendModel
+    {
+        public string Name { get; set; }
+        public string Profile { get; set; }
+    }
+}
