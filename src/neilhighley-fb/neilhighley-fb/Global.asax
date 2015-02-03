5608ef8a02b0d2774a4c0ce878425ab36b43a6ea
Neil Highley <neil@neilhighley.com>
2015-02-03 19:31:28 +0000

    init

---
+++ src/neilhighley-fb/neilhighley-fb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="neilhighley_fb.MvcApplication" Language="C#" %>
---
+++ src/neilhighley-fb/neilhighley-fb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(neilhighley_fb.Startup))]
+namespace neilhighley_fb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
